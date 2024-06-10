85a16ad1d162546b7601b178ef55d6def9614543
hilmanahmad <72850834+hilmanahmad@users.noreply.github.com>
2024-06-10 22:29:46 +0700

    Init project. Register user endpoint

---
+++ user/entity.go
@@ -0,0 +1,17 @@
+package user
+
+import (
+	"time"
+)
+
+type User struct {
+	ID           int `json:"id"`
+	Name         string `json:"name"`
+	Occupation   string `json:"occupation"`
+	Email        string `json:"email"`
+	PasswordHash string `json:"password_hash"`
+	Role         string `json:"role"`
+	Token        string `json:"token"`
+	CreatedAt    time.Time `json:"created_at"`
+	UpdatedAt 	 time.Time `json:"updated_at"`
+}
