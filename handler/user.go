76d3513890f59cface3c1075a7d84c019c2e6dd1
hilmanahmad <72850834+hilmanahmad@users.noreply.github.com>
2024-06-13 13:53:17 +0700

    Login endpoint

--- handler/user.go
+++ handler/user.go
@@ -41,3 +41,29 @@ formatter := user.FormatterUser(newUser, "token")
 response := helper.APIResponse("Account has been registered", http.StatusOK, "success", formatter)
 c.JSON(http.StatusOK, response)
 }
+
+func (h *userHandler) Login(c *gin.Context) {
+	var input user.LoginInput
+
+	err := c.ShouldBindJSON(&input)
+	if err != nil {
+		errors := helper.FormatValidationError(err)
+	errorMessage := gin.H{"errors": errors}
+response := helper.APIResponse("Login failed", http.StatusBadRequest, "error", errorMessage)
+	c.JSON(http.StatusUnprocessableEntity, response)
+	return
+	}
+
+	loggedinUser, err := h.userService.Login(input)
+	if err != nil {
+		errorMessage := gin.H{"errors": err.Error()}
+		response := helper.APIResponse("Login failed", http.StatusUnprocessableEntity, "error", errorMessage)
+		c.JSON(http.StatusUnprocessableEntity, response)
+	return
+	}
+
+	formatter := user.FormatterUser(loggedinUser, "tokenss")
+
+	response := helper.APIResponse("Successfuly logedin", http.StatusOK, "success", formatter)
+		c.JSON(http.StatusOK, response)
+}
