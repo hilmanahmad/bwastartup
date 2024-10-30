6d9d70cbfd2fee96548096e2e0a50870240d5bac
hilmanahmad <72850834+hilmanahmad@users.noreply.github.com>
2024-10-30 14:28:36 +0700

    update

--- handler/transaction.go
+++ handler/transaction.go
@@ -2,7 +2,6 @@ package handler
 
 import (
 	"bwastartup/helper"
-	"bwastartup/payment"
 	"bwastartup/transaction"
 	"bwastartup/user"
 	"github.com/gin-gonic/gin"
@@ -10,12 +9,11 @@ import (
 )
 
 type TransactionHandler struct {
-	service        transaction.Service
-	paymentService payment.Service
+	service transaction.Service
 }
 
-func NewTransactionHandler(service transaction.Service, paymentService payment.Service) *TransactionHandler {
-	return &TransactionHandler{service, paymentService}
+func NewTransactionHandler(service transaction.Service) *TransactionHandler {
+	return &TransactionHandler{service}
 }
 func (h *TransactionHandler) GetCampaignTransactions(c *gin.Context) {
 	var input transaction.GetCampaignTransactionsInput
@@ -89,7 +87,7 @@ func (h *TransactionHandler) GetNotification(c *gin.Context) {
 
 		return
 	}
-	err = h.paymentService.ProcessPayment(input)
+	err = h.service.ProcessPayment(input)
 	if err != nil {
 		response := helper.APIResponse("Failed to process notification", http.StatusBadRequest, "error", nil)
 		c.JSON(http.StatusBadRequest, response)
